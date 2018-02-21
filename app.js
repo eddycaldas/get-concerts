43c310603dc6b7cece2d43cf0563a9c1b5270bcd
Eddy Caldas <eddycaldas@live.com>
2018-02-20 20:46:17 -0700

    html and css initiated

---
+++ app.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $('#button').click(function(e) {
+    var textInput = $("#input").val();
+    var api = "http://api.openweathermap.org/data/2.5/weather?q="
+    var format = "&units=imperial"
+    var key = "&APPID=25b486c97325bf6814cc4456fabac99e"
+    // api + city + format + key
+    
+    
+    
+    
+    
+    e.preventDefault();
+  })
+});
+
